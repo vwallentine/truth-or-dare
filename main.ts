59c9cea7102a3fb867575cf1005e3ac559a5f88b
vwallentine <112982505+vwallentine@users.noreply.github.com>
2022-09-13 13:03:53 -0600

    Truth or Dare game

--- main.ts
+++ main.ts
@@ -1,15 +1,49 @@
-input.onGesture(Gesture.Shake, function () {
-    RandomNumber = randint(0, 2)
-    if (RandomNumber == 0) {
-        basic.showString("Answer Unclear")
-    } else if (RandomNumber == 1) {
-        basic.showString("No")
-    } else {
-        basic.showString("Yes")
+input.onButtonPressed(Button.A, function () {
+    Dog = randint(0, 1)
+    if (Dog == 0) {
+        basic.showString("Truth")
+    } else if (Dog == 1) {
+        basic.showString("Dare")
     }
 })
-let RandomNumber = 0
-basic.showString("8 Ball")
-basic.forever(function () {
-	
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    Horse = randint(0, 3)
+    if (Horse == 0) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # # # # #
+            . . # . .
+            . . # . .
+            `)
+    } else if (Horse == 1) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # # # # #
+            . # # # .
+            . . # . .
+            `)
+    } else if (Horse == 2) {
+        basic.showLeds(`
+            . . # . .
+            . # # . .
+            # # # # #
+            . # # . .
+            . . # . .
+            `)
+    } else if (Horse == 3) {
+        basic.showLeds(`
+            . . # . .
+            . . # # .
+            # # # # #
+            . . # # .
+            . . # . .
+            `)
+    }
+    basic.clearScreen()
 })
+let Horse = 0
+let Dog = 0
+basic.showString("Truth or Dare")
