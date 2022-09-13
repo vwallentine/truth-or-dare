797a6399700d1931eee4d554f424452c5f5cb340
vwallentine <112982505+vwallentine@users.noreply.github.com>
2022-09-13 12:37:18 -0600

    Loops and Booleans

--- main.ts
+++ main.ts
@@ -1,7 +1,15 @@
-for (let index = 0; index < 5; index++) {
-    basic.showIcon(IconNames.Happy)
-    basic.showIcon(IconNames.Sad)
-}
+input.onGesture(Gesture.Shake, function () {
+    RandomNumber = randint(0, 2)
+    if (RandomNumber == 0) {
+        basic.showString("Answer Unclear")
+    } else if (RandomNumber == 1) {
+        basic.showString("No")
+    } else {
+        basic.showString("Yes")
+    }
+})
+let RandomNumber = 0
+basic.showString("8 Ball")
 basic.forever(function () {
 	
 })
